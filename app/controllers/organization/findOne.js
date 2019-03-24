c501f94413e050285f9445752df25542344790aa
Rudraprasad Das <brayn003@gmail.com>
2019-03-24 18:51:50 +0530

    minor fixes in code

--- app/controllers/organization/findOne.js
+++ app/controllers/organization/findOne.js
@@ -3,6 +3,6 @@ const Organization = require('~models/Organization');
 module.exports = async (req, res) => {
   const { params } = req;
   const { id } = params;
-  const organization = Organization.findById(id);
-  return res.status(200).json(organization);
+  const organization = await Organization.findById(id);
+  return res.status(200).json(organization.toJSON({ virtuals: true }));
 };
