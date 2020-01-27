866804474ab127bba4c5a28653efbf8527c445ff
Rudraprasad Das <brayn003@gmail.com>
2020-01-27 13:34:38 +0530

    adding cashflow bar

--- app/controllers/dashboard/cashflowBar.js
+++ app/controllers/dashboard/cashflowBar.js
@@ -15,6 +15,20 @@ const controller = async (req, res) => {
   const now = new Date();
   const month = parseInt(query.month, 10) || now.getMonth() + 1;
   const year = parseInt(query.year, 10) || now.getFullYear();
+
+
+  const expectedPeriods = [{ month, year }];
+  if (month === 1) {
+    expectedPeriods.push({ month: 12, year: year - 1 });
+    expectedPeriods.push({ month: 11, year: year - 1 });
+  } else if (month === 2) {
+    expectedPeriods.push({ month: 1, year });
+    expectedPeriods.push({ month: 12, year: year - 1 });
+  } else {
+    expectedPeriods.push({ month: month - 1, year });
+    expectedPeriods.push({ month: month - 2, year });
+  }
+
   const $match = {};
   if (query.organization) $match.organization = mongoose.Types.ObjectId(query.organization);
   let aggIncoming = [];
@@ -39,6 +53,7 @@ const controller = async (req, res) => {
           total: '$roundedAmountReceivable',
         },
       },
+      { $match: { $or: expectedPeriods } },
       {
         $group: {
           _id: {
@@ -70,6 +85,7 @@ const controller = async (req, res) => {
           total: '$roundedAmountPayable',
         },
       },
+      { $match: { $or: expectedPeriods } },
       {
         $group: {
           _id: {
@@ -86,35 +102,62 @@ const controller = async (req, res) => {
     throw new ValidationError(err.errmsg);
   }
 
-  const expectedPeriods = [{ month, year }];
-  if (month === 1) {
-    expectedPeriods.push({ month: 12, year: year - 1 });
-    expectedPeriods.push({ month: 11, year: year - 1 });
-  } else if (month === 2) {
-    expectedPeriods.push({ month: 1, year });
-    expectedPeriods.push({ month: 12, year: year - 1 });
-  } else {
-    expectedPeriods.push({ month: month - 1, year });
-    expectedPeriods.push({ month: month - 2, year });
-  }
+
+  const data = [];
+  aggIncoming.reduce((agg, inc) => {
+    const { _id, remaining, received } = inc;
+    agg.push({
+      Month: `${months[_id.month - 1]} ${_id.year}`,
+      // Year: _id.year,
+      Type: 'Income',
+      Status: 'Pending',
+      Amount: remaining,
+    });
+    agg.push({
+      Month: `${months[_id.month - 1]} ${_id.year}`,
+      // Year: _id.year,
+      Type: 'Income',
+      Status: 'Confirmed',
+      Amount: received,
+    });
+    return agg;
+  }, data);
+  aggOutgoing.reduce((agg, out) => {
+    const { _id, remaining, paid } = out;
+    agg.push({
+      Month: `${months[_id.month - 1]} ${_id.year}`,
+      // Year: _id.year,
+      Type: 'Expense',
+      Status: 'Pending',
+      Amount: remaining,
+    });
+    agg.push({
+      Month: `${months[_id.month - 1]} ${_id.year}`,
+      // Year: _id.year,
+      Type: 'Expense',
+      Status: 'Confirmed',
+      Amount: paid,
+    });
+    return agg;
+  }, data);
 
   // FUCKED UP CODE
-  const data = aggIncoming
-    // .sort((a, b) => ((b.year * 100) + b.month) - ((a.year * 100) + a.month))
-    .filter(({ _id: inc }) => (
-      !!expectedPeriods.find(p => ((p.month === inc.month) && (p.year === inc.year)))
-    ))
-    .map(inc => ({
-      Month: `${months[inc.month - 1]} ${inc.year}`,
-      // Amount: inc.
-    }));
+  // const data = aggIncoming;
+  // .sort((a, b) => ((b.year * 100) + b.month) - ((a.year * 100) + a.month))
+  // .filter(({ _id: inc }) => (
+  //   !!expectedPeriods.find(p => ((p.month === inc.month) && (p.year === inc.year)))
+  // ))
+  // .map(inc => ({
+  //   Month: `${months[inc.month - 1]} ${inc.year}`,
+  //   // Amount: inc.
+  // }));
 
   return res.status(200).json({
     month,
     year,
     valueKey: 'Amount',
     labelKey: 'Month',
-    groupKey: 'Type',
+    groupBy: 'Type',
     stackBy: 'Status',
     data,
   });
